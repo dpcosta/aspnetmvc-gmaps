24b4fc759c137db20e2fb6a1dfbe8aef0ce95a5f
dpcosta <dpcosta@gmail.com>
2017-09-18 16:24:18 -0300

    Adicionar arquivos de projeto.

---
+++ Alura.GoogleMaps.Web/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Alura.GoogleMaps.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Alura.GoogleMaps.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var coordenadas = new Coordenada
+            {
+                Latitude = "-23.64340873969638",
+                Longitude = "-46.730219057147224"
+            };
+            return View(coordenadas);
+        }
+    }
+}
---
+++ Alura.GoogleMaps.Web/Models/Coordenadas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Alura.GoogleMaps.Web.Models
+{
+    public class Coordenada
+    {
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+    }
+}
