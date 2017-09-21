2b244f28a02a2eb9bdc0b9c5547784562e2619cb
dpcosta <dpcosta@gmail.com>
2017-09-21 14:17:47 -0300

    - formulário de localização de aeroportos próximos - marcadores no mapa

--- Alura.GoogleMaps.Web/Controllers/HomeController.cs
+++ Alura.GoogleMaps.Web/Controllers/HomeController.cs
@@ -1,9 +1,15 @@
 ﻿using Alura.GoogleMaps.Web.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net;
+using Newtonsoft.Json;
+using Alura.GoogleMaps.Web.Geocoding;
 
 namespace Alura.GoogleMaps.Web.Controllers
 {
@@ -12,12 +18,48 @@ namespace Alura.GoogleMaps.Web.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            var coordenadas = new Coordenada
-            {
-                Latitude = "-23.64340873969638",
-                Longitude = "-46.730219057147224"
-            };
+            //coordenadas quaisquer para mostrar o mapa
+            var coordenadas = new Coordenada("São Paulo", "-23.64340873969638", "-46.730219057147224");
             return View(coordenadas);
+        }
+
+        public JsonResult Localizar(HomeViewModel model)
+        {
+            Debug.WriteLine(model);
+
+            //usar geocoding para obter as coordenadas da localização:
+            Coordenada coordLocal = ObterCoordenadasDaLocalizacao(model.Endereco);
+
+            //buscar no banco de dados os aeroportos próximos daquela localização
+            //atualmente está gerando uma lista fixa
+            var coordenadas = new List<Coordenada>
+            {
+                coordLocal,
+                new Coordenada("Santos Dumont", "-22.9111438", "-43.1670642"),
+                new Coordenada("Galeão", "-22.8134098", "-43.251612")
+
+            };
+            return Json(coordenadas);
+        }
+
+        private Coordenada ObterCoordenadasDaLocalizacao(string endereco)
+        {
+            string url = $"http://maps.google.com/maps/api/geocode/json?address={endereco}";
+            Debug.WriteLine(url);
+
+            var coord = new Coordenada("Não Localizado", "-10", "-10");
+            var response = new WebClient().DownloadString(url);
+            var googleGeocode = JsonConvert.DeserializeObject<GoogleGeocodeResponse>(response);
+            //Debug.WriteLine(googleGeocode);
+
+            if (googleGeocode.status == "OK")
+            {
+                coord.Nome = googleGeocode.results[0].formatted_address;
+                coord.Latitude = googleGeocode.results[0].geometry.location.lat;
+                coord.Longitude = googleGeocode.results[0].geometry.location.lng;
+            }
+
+            return coord;
         }
     }
 }
--- Alura.GoogleMaps.Web/Models/Coordenadas.cs
+++ Alura.GoogleMaps.Web/Models/Coordenadas.cs
@@ -7,7 +7,15 @@ namespace Alura.GoogleMaps.Web.Models
 {
     public class Coordenada
     {
+        public string Nome { get; set; }
         public string Latitude { get; set; }
         public string Longitude { get; set; }
+
+        public Coordenada(string nome, string lat, string lng)
+        {
+            Nome = nome;
+            Latitude = lat;
+            Longitude = lng;
+        }
     }
 }
